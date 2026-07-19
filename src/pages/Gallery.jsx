f4609e915812d79f265f6a43f7e17b432e5aa90c
ankit871331 <ankit871331@gmail.com>
2026-07-20 02:15:41 +0530

    galary

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -13,7 +13,7 @@ export default function Gallery() {
   useEffect(() => {
     const fetchGallery = async () => {
       try {
-        const res = await axios.get(`${API}/gallery`);
+        const res = await axios.get(`${API}/api/gallery`);
         setImages(res.data);
       } catch (err) {
         console.error('Error fetching gallery:', err);
